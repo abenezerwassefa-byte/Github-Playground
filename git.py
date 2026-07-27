2d7d6ef652446e61bef705f3baf6ac7676942f48
Abenezer W. Assefa <abenezer.w.assefa@gmail.com>
2026-07-27 17:04:52 +0300

    Add Car class and instantiate car objects in git.py

--- git.py
+++ git.py
@@ -1,1 +1,7 @@
-print('stuff')
+from car import Car
+
+car1 = Car('BMW', 2026, 'Matte Black', True)
+car2 = Car('Mustang', 2025, 'Gray', False)
+car3 = Car('V-Wagon', 2026, 'BLACK', False)
+
+car1.stop()
