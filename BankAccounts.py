d90c23ebff7099cf9b8369b8b4c76ce461041f72
Abenezer W. Assefa <abenezer.w.assefa@gmail.com>
2026-07-30 15:49:56 +0300

    Add withdraw method and balance validation to BankAccount class

--- BankAccounts.py
+++ BankAccounts.py
@@ -1,3 +1,7 @@
+class BalanceException(Exception):
+    pass
+
+
 class BankAccount:
     def __init__(self, initialAmount, acctName):
         self.balance = initialAmount
@@ -13,3 +17,21 @@ class BankAccount:
         self.amount = amount
         print(f"Deposit complete. ${self.amount} deposited.")
         self.getBalance()
+
+# Now, let's do a withdraw method. This could get a little complex.
+    def viableTransaction(self, amount):
+        if self.balance >= amount:
+            return
+        else:
+            raise BalanceException(
+                f"Sorry, your account only has ${self.balance}"
+            )
+
+    def withdraw(self, amount):
+        try:
+            self.viableTransaction(amount)
+            self.balance = self.balance - amount
+            print("\nWithdraw complete.")
+            self.getBalance()
+        except BalanceException as error:
+            print(f"\nWithdraw interrupted: {error}")
--- oop_project.py
+++ oop_project.py
@@ -3,3 +3,4 @@ Dave = BankAccount(1000.2312, "Dave")
 Sara = BankAccount(2000, "Sara")
 
 Dave.deposit(400)
+Dave.withdraw(1000)
