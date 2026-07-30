9b04ac75f0eca76167c1a56a7761e47c83fc9388
Abenezer W. Assefa <abenezer.w.assefa@gmail.com>
2026-07-30 14:35:20 +0300

    Fix account creation message to use print instead of return; add deposit method functionality

--- BankAccounts.py
+++ BankAccounts.py
@@ -2,4 +2,14 @@ class BankAccount:
     def __init__(self, initialAmount, acctName):
         self.balance = initialAmount
         self.name = acctName
-        return f"Account {self.name} has been created. Your balance is ${self.balance:.2f}"
+        print(
+            f"Account {self.name} has been created. Your balance is ${self.balance:.2f}")
+
+    def getBalance(self):
+        print(f"Account {self.name} has ${self.balance:.2f}")
+
+    def deposit(self, amount):
+        self.balance = self.balance + amount
+        self.amount = amount
+        print(f"Deposit complete. ${self.amount} deposited.")
+        self.getBalance()
