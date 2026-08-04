ff9956f68a233b393a6b96da8e000d694f8d8b97
Abenezer W. Assefa <abenezer.w.assefa@gmail.com>
2026-08-04 12:55:32 +0300

    Remove oop_project.py and its associated BankAccount functionality

--- oop_project.py
+++
@@ -1,9 +0,0 @@
-from BankAccounts import *
-Dave = BankAccount(1000.2312, "Dave")
-Sara = BankAccount(2000, "Sara")
-
-Jim = Interest(50000, "Jimmy")
-Jim.getBalance()
-Jim.deposit(100)
-Jim.transfer(200, Dave)
-# ============Study this!! Don't slack off!!
