8fd7deaf8061d72e09a43f218abef44ed81b8221
Abenezer W. Assefa <abenezer.w.assefa@gmail.com>
2026-08-04 12:51:24 +0300

    Refactor task management application: replace BankAccounts.py with T_Flow.py and update templates

---
+++ T_Flow.py
@@ -0,0 +1,59 @@
+# imports
+from flask import Flask, render_template, redirect, request
+from flask_scss import Scss
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, timezone
+app = Flask(__name__)
+Scss(app)
+
+# learn what sqlalchemy is!!
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+db = SQLAlchemy(app)
+
+# Data class ~ Row of data
+# One model = one row of data
+
+
+class MyTask(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(100), nullable=False)
+    complete = db.Column(db.Integer)
+    created = db.Column(db.DateTime, default=datetime.now(timezone.utc))
+    # DateTime is an object here
+
+    def __repr__(self):
+        return f"Task{self.id}"
+
+# Routes to Webpages
+
+
+# POST MEANS SEND DATA AND GET MEANS RECIEVE DATA
+@app.route("/", methods=["POST", "GET"])
+def index():
+    # Add tasks
+    if request.method == "POST":
+        current_task = request.form
+        # UNDERSTAND THIS SECTION WELL.
+        new_task = MyTask(content=current_task)
+        try:  # IF YOU HAVE A TRY BLOCK, YOU SHOULD HAVE AN EXCEPTION JUST IN CASE YOU NEED TO HANDLE AND ERROR.
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect("/")
+        except Exception as e:
+            print("Error:{e}")
+            # WHAT IS THE USE OF THIS LINE IF WE HAVE A PRINT STATEMENT THAT DOES THE SAME THING?
+            return f"ERROR:{e}"
+    # See all added tasks
+    else:
+        tasks = MyTask.query > order
+    return render_template("Todo.html")
+
+
+# Runner and Debugger
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+
+    app.run(debug=True)
+
+# above is a flask tutorial attempt^^^^
